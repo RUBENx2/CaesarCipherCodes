b6d26c1d75942dab5163361a24c2052d14cf21c3
Ruben Benitez <125417446+RUBENx2@users.noreply.github.com>
2023-09-13 18:49:26 -0400

    Update CaesarCipher.py

--- CaesarCipher.py
+++ CaesarCipher.py
@@ -1,19 +1,24 @@
-import string
+def encrypt_text(plaintext, n):
+    ans = ""
+    # iterate over the given text
+    for i in range(len(plaintext)):
+        ch = plaintext[i]
 
-Letters = string.ascii_lowercase
+        # check if space is there then simply add space
+        if ch == " ":
+            ans += " "
+        # check if a character is uppercase then encrypt it accordingly
+        elif ch.isupper():
+            ans += chr((ord(ch) + n - 65) % 26 + 65)
+        # check if a character is lowercase then encrypt it accordingly
+        else:
+            ans += chr((ord(ch) + n - 97) % 26 + 97)
 
-# input field for the encryoted message, inputting "lower" makes it so it treats uppercase letters as lowercase
-message = input("Enter the encrypted message: ").lower() 
+    return ans
 
-for key in range(len(Letters)):
-   translated = ''
-   for ch in message:
-      if ch in Letters:
-         num = Letters.find(ch)
-         num = num - key
-         if num < 0:
-            num = num + len(Letters)
-         translated = translated + Letters[num]
-      else:
-         translated = translated + ch
-   print('Decryption key is %s: %s' % (key, translated))
+# Take input from the user for plaintext and shift pattern (n)
+plaintext = input("Enter the plaintext: ")
+n = int(input("Enter the shift pattern: "))
+
+print("-------------PIZZA PIZZA----------------")
+print("Cipher Text is : " + encrypt_text(plaintext, n))
