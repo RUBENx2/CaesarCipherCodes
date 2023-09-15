e89d0c0d22ebdcde58c7047fbc333421148a812e
Ruben Benitez <125417446+RUBENx2@users.noreply.github.com>
2023-09-14 20:02:08 -0400

    Update CaesarCipher.py

--- CaesarCipher.py
+++ CaesarCipher.py
@@ -1,16 +1,16 @@
 def encrypt_text(plaintext, n):
     ans = ""
-    # iterate over the given text
+    
     for i in range(len(plaintext)):
         ch = plaintext[i]
 
-        # check if space is there then simply add space
+        # check if space is there, then simply add space
         if ch == " ":
             ans += " "
-        # check if a character is uppercase then encrypt it accordingly
+        # check if a character is uppercase 
         elif ch.isupper():
             ans += chr((ord(ch) + n - 65) % 26 + 65)
-        # check if a character is lowercase then encrypt it accordingly
+        # check if a character is lowercase 
         else:
             ans += chr((ord(ch) + n - 97) % 26 + 97)
 
